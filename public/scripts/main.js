1a53fec075b097e2d8f0b9d626133caf4e7c17ef
rhit-greenbmj <greenberg.math@gmail.com>
2022-05-04 02:00:36 -0400

    Finished login and fixed file hierarchy

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -50,7 +50,7 @@ rhit.FbAuthManager = class {
 		});
 	}
 	signIn() {
-		Rosefire.signIn("d9097b0f-a3f6-4423-84b6-0824d063a615", (err, rfUser) => {
+		Rosefire.signIn("88a14583-be71-448d-88a9-96272fead15e", (err, rfUser) => {
 			if (err) {
 				console.log("Rosefire error!", err);
 				return;
@@ -82,59 +82,73 @@ rhit.FbAuthManager = class {
 	}
 }
 
-rhit.initializePage = function() {
-    if (document.querySelector("#loginPage")) {
-        console.log("Login Page");
-        new rhit.LoginPageController();
-    }
+rhit.checkForRedirects = function () {
+	console.log("In checkforredirects");
+	if (document.querySelector("#loginPage") && rhit.fbAuthManager.isSignedIn) {
+		console.log("here");
+		window.location.href = "/userpage.html";
+	}
+	// if (document.querySelector("#mainPage") && !rhit.fbAuthManager.isSignedIn) {
+	//     window.location.href = "/userpage.html";
+	// }
 }
 
-rhit.checkForRedirects = function() {
-    if (document.querySelector("#loginPage") && rhit.fbAuthManager.isSignedIn) {
-        // window.location.href = "/index.html";
-    }
-    // if (document.querySelector("#mainPage") && !rhit.fbAuthManager.isSignedIn) {
-    //     window.location.href = "/userpage.html";
-    // }
+rhit.initializePage = function () {
+	if (document.querySelector("#loginPage")) {
+		console.log("Login Page");
+		rhit.fbAuthManager = new rhit.FbAuthManager();
+		rhit.fbAuthManager.beginListening((params) => {
+			console.log("isSignedIn: ", rhit.fbAuthManager.isSignedIn);
+		});
+		new rhit.LoginPageController();
+	} else if (document.querySelector("#userPage")) {
+		document.getElementById("newBuild").onclick = (event) => {
+			window.location.href = "/create.html";
+		};
+	} else if (document.querySelector("#mainPage")) {
+		document.getElementById("closeSplash").onclick = (event) => {
+			document.getElementById("splashbg").style.display = "none";
+		}
+	}
+
+	if (!document.querySelector("#loginPage")) {
+		rhit.navManager = new rhit.NavManager();
+
+		document.querySelector(".navButton").onclick = (event) => {
+			if (!rhit.navManager.panelopen) {
+				rhit.navManager.openNav();
+				rhit.navManager.panelopen = true;
+			} else {
+				rhit.navManager.closeNav();
+				rhit.navManager.panelopen = false;
+			}
+		};
+	}
+	rhit.checkForRedirects();
+}
+
+rhit.NavManager = class {
+	constructor() {
+		let panelopen = false;
+	}
+
+	openNav() {
+		document.getElementById("sidePanel").style.width = "300px";
+	}
+	closeNav() {
+		document.getElementById("sidePanel").style.width = "0";
+	}
+
 }
 
 /* Main */
 /** function and class syntax examples */
 rhit.main = function () {
-	// rhit.fbAuthManager = new rhit.FbAuthManager();
-    // rhit.fbAuthManager.beginListening((params) => {
-    //     console.log("isSignedIn: ", rhit.fbAuthManager.isSignedIn);
-    //     rhit.checkForRedirects();
-    //     rhit.initializePage();
-	// });
+
+	rhit.initializePage();
+
 	console.log("Ready");
 
-	// THESE NEED TO BE PUT IN RESPECTIVE CLASSES
-	// Side Panel Open and Close
-	let panelopen = false;
-	function openNav() {
-		document.getElementById("sidePanel").style.width = "300px";
-	}
-	function closeNav() {
-		document.getElementById("sidePanel").style.width = "0";
-	}
-	document.getElementById("panelButton").onclick = (event) => {
-		if(!panelopen) {
-			openNav();
-			panelopen = true;
-		} else {
-			closeNav();
-			panelopen = false;
-		}
-	};
-	document.getElementById("newBuild").onclick = (event) => {
-		window.location.href = "../public/create.html";
-	};
-
-	// Welcome Splash Modal
-	document.getElementById("closeSplash").onclick = (event) => {
-		document.getElementById("splashbg").style.display = "none";
-	}
 };
 
 rhit.main();
